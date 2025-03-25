4ca105cdefd4ad961ec0a5db117c7c958b7c3336
Alexander Sapountzis <alexs@mparticle.com>
2025-03-25 15:30:23 -0400

    Update test cases for ES6

--- test/src/tests.js
+++ test/src/tests.js
@@ -1,74 +1,23 @@
-/* eslint-disable no-undef*/
-describe('Rokt Forwarder', function () {
-    // -------------------DO NOT EDIT ANYTHING BELOW THIS LINE-----------------------
-    var // MessageType = {
-        //     SessionStart: 1,
-        //     SessionEnd: 2,
-        //     PageView: 3,
-        //     PageEvent: 4,
-        //     CrashReport: 5,
-        //     OptOut: 6,
-        //     AppStateTransition: 10,
-        //     Profile: 14,
-        //     Commerce: 16
-        // },
-        // EventType = {
-        //     Unknown: 0,
-        //     Navigation: 1,
-        //     Location: 2,
-        //     Search: 3,
-        //     Transaction: 4,
-        //     UserContent: 5,
-        //     UserPreference: 6,
-        //     Social: 7,
-        //     Other: 8,
-        //     Media: 9,
-        //     getName: function() {
-        //         return 'blahblah';
-        //     }
-        // },
-        // ProductActionType = {
-        //     Unknown: 0,
-        //     AddToCart: 1,
-        //     RemoveFromCart: 2,
-        //     Checkout: 3,
-        //     CheckoutOption: 4,
-        //     Click: 5,
-        //     ViewDetail: 6,
-        //     Purchase: 7,
-        //     Refund: 8,
-        //     AddToWishlist: 9,
-        //     RemoveFromWishlist: 10
-        // },
-        IdentityType = {
-            Other: 0,
-            CustomerId: 1,
-            Facebook: 2,
-            Twitter: 3,
-            Google: 4,
-            Microsoft: 5,
-            Yahoo: 6,
-            Email: 7,
-            Alias: 8,
-            FacebookCustomAudienceId: 9,
-        },
-        ReportingService = function () {
-            var self = this;
+/* eslint-env es6, mocha */
+/* eslint-parser babel-eslint */
+describe('Rokt Forwarder', () => {
+    var ReportingService = function () {
+        var self = this;
 
+        this.id = null;
+        this.event = null;
+
+        this.cb = function (forwarder, event) {
+            self.id = forwarder.id;
+            self.event = event;
+        };
+
+        this.reset = function () {
             this.id = null;
             this.event = null;
-
-            this.cb = function (forwarder, event) {
-                self.id = forwarder.id;
-                self.event = event;
-            };
-
-            this.reset = function () {
-                this.id = null;
-                this.event = null;
-            };
-        },
-        reportService = new ReportingService();
+        };
+    };
+    var reportService = new ReportingService();
 
     // -------------------DO NOT EDIT ANYTHING ABOVE THIS LINE-----------------------
     // -------------------START EDITING BELOW:-----------------------
@@ -86,21 +35,10 @@ describe('Rokt Forwarder', function () {
     var MockRoktForwarder = function () {
         var self = this;
 
-        // create properties for each type of event you want tracked, see below for examples
-        this.trackCustomEventCalled = false;
-        this.logPurchaseEventCalled = false;
         this.initializeCalled = false;
 
-        this.trackCustomName = null;
-        this.logPurchaseName = null;
         this.accountId = null;
         this.sandbox = null;
-        this.userId = null;
-        this.userAttributes = {};
-        this.userIdField = null;
-
-        this.eventProperties = [];
-        this.purchaseEventProperties = [];
 
         this.createLauncherCalled = false;
         this.createLauncher = function (options) {
@@ -116,54 +54,18 @@ describe('Rokt Forwarder', function () {
         };
 
         this.currentLauncher = function () {};
-
-        // stub your different methods to ensure they are being called properly
-        this.initialize = function (appId, apiKey) {
-            self.initializeCalled = true;
-            self.apiKey = apiKey;
-            self.appId = appId;
-        };
-
-        this.stubbedTrackingMethod = function (name, eventProperties) {
-            self.trackCustomEventCalled = true;
-            self.trackCustomName = name;
-            self.eventProperties.push(eventProperties);
-            // Return true to indicate event should be reported
-            return true;
-        };
-
-        this.stubbedUserAttributeSettingMethod = function (userAttributes) {
-            self.userId = id;
-            userAttributes = userAttributes || {};
-            if (Object.keys(userAttributes).length) {
-                for (var key in userAttributes) {
-                    if (userAttributes[key] === null) {
-                        delete self.userAttributes[key];
-                    } else {
-                        self.userAttributes[key] = userAttributes[key];
-                    }
-                }
-            }
-        };
-
-        this.stubbedUserLoginMethod = function (id) {
-            self.userId = id;
-        };
     };
 
-    before(function () {
-    });
+    before(() => {});
 
-    beforeEach(function () {
-    });
+    beforeEach(() => {});
 
-    it.only('should initialize the Rokt Web SDK', async () => {
+    it('should initialize the Rokt Web SDK', async () => {
         window.Rokt = new MockRoktForwarder();
+        window.mParticle.Rokt = window.Rokt;
         window.mParticle.Rokt.attachLauncherCalled = false;
-        window.mParticle.Rokt.attachLauncher = async (options) => {
-            console.log('attachLauncher called', options);
+        window.mParticle.Rokt.attachLauncher = async () => {
             window.mParticle.Rokt.attachLauncherCalled = true;
-            console.log('checking attachLauncherCalled top', window.mParticle.Rokt.attachLauncherCalled);
             Promise.resolve();
         };
 
@@ -181,7 +83,9 @@ describe('Rokt Forwarder', function () {
 
         window.Rokt.createLauncherCalled.should.equal(true);
 
-        await waitForCondition(() => window.mParticle.Rokt.attachLauncherCalled === true);
+        await waitForCondition(
+            () => window.mParticle.Rokt.attachLauncherCalled === true
+        );
     });
 });
 
