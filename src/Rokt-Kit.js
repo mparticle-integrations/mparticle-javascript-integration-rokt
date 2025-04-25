170a8bb54d9649fb3fb3cfedddb022d801a6607b
Alexander Sapountzis <alexs@mparticle.com>
2025-04-25 11:29:04 -0400

    Address PR Comments

--- src/Rokt-Kit.js
+++ src/Rokt-Kit.js
@@ -249,7 +249,6 @@ var constructor = function () {
                 window.mParticle.Rokt.attachKit(self);
 
                 self.isInitialized = true;
-                console.warn('Rokt TRACE: launcher called');
             })
             .catch(function (err) {
                 console.error('Error creating Rokt launcher:', err);
