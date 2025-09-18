deda76f0a9840ef17ae574db7a94a2e7d73c3e79
Robert Ing <rob.ing@rokt.com>
2025-09-18 14:34:22 -0400

    remove unused function

--- src/Rokt-Kit.js
+++ src/Rokt-Kit.js
@@ -479,8 +479,6 @@ var constructor = function () {
     }
 };
 
-function getOtherIdentityType(settings) {}
-
 function generateIntegrationName(customIntegrationName) {
     var coreSdkVersion = window.mParticle.getVersion();
     var kitVersion = process.env.PACKAGE_VERSION;
